9d7a5a279ad207393f7f65c4725e84f7d711e8a6
은지\ohohd <ohohdmswl@naver.com>
2023-06-15 19:14:39 +0900

    today

---
+++ src/main/java/kr/happyjob/study/accAcm/dao/accAcmDao.java
@@ -0,0 +1,21 @@
+package kr.happyjob.study.accAcm.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import kr.happyjob.study.accAcm.model.accAcmModel;
+import kr.happyjob.study.accAcs.model.accAcsModel;
+import kr.happyjob.study.mngNot.model.NoticeModel;
+
+public interface accAcmDao {
+
+//	List<accAcmModel> accAcmSearchList(Map<String, Object> paramMap);
+
+
+
+	
+	
+	
+}
